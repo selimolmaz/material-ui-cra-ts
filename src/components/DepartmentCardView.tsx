36400a2f3b3443eb038e0634d978522f9eafaeb5
selim-olmaz <selimolmaz3.1415@gmail.com>
2024-11-26 17:25:24 +0300

    Artık derpartmanları kartlar olarak gösterceğiz

---
+++ src/components/DepartmentCardView.tsx
@@ -0,0 +1,51 @@
+import { Button, Card, CardActions, CardContent, Typography } from '@mui/material';
+import * as React from 'react';
+import DepartmentDTO from '../models/DepartmentDTO';
+import { useState } from 'react';
+import EditDepartmentModal from './EditDepartment';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    p: 4,
+};
+
+interface DepartmentCardViewProps {
+    department: DepartmentDTO;
+}
+
+export default function DepartmentCardView({ department }: DepartmentCardViewProps) {
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const handleOpenModal = () => setModalOpen(true);
+    const handleCloseModal = () => setModalOpen(false);
+
+    return (
+        <Card sx={{ minWidth: 222 }}>
+            <CardContent>
+                <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
+                    {department.deptName}
+                </Typography>
+                <Typography variant="h5" component="div">
+                    {department.building}
+                </Typography>
+                <Typography variant="body2">
+                    {department.budget}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small" onClick={handleOpenModal}>Edit</Button>
+            </CardActions>
+            <EditDepartmentModal
+                department={department}
+                open={modalOpen}
+                onClose={handleCloseModal}
+            />
+        </Card>
+    );
+}
