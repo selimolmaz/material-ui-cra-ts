20e99134653643a899cece266fca204f2cae241d
selim-olmaz <selimolmaz3.1415@gmail.com>
2024-11-27 00:27:43 +0300

    artık seçilen öğrencileri de öğrenmek istiyorum

--- src/context/StudentContext.tsx
+++ src/context/StudentContext.tsx
@@ -1,15 +1,18 @@
-
 import React, { createContext, useState } from 'react';
 import StudentDTO from '../models/StudentDTO';
 
 interface StudentContextProps {
     students: StudentDTO[];
     setStudents: React.Dispatch<React.SetStateAction<StudentDTO[]>>;
+    selectedStudents: StudentDTO[];
+    setSelectedStudents: React.Dispatch<React.SetStateAction<StudentDTO[]>>;
 }
 
 export const StudentContext = createContext<StudentContextProps>({
     students: [],
     setStudents: () => {},
+    selectedStudents: [],
+    setSelectedStudents: () => {},
 });
 
 interface StudentProviderProps {
@@ -18,9 +21,10 @@ interface StudentProviderProps {
 
 export const StudentProvider: React.FC<StudentProviderProps> = ({ children }) => {
     const [students, setStudents] = useState<StudentDTO[]>([]);
+    const [selectedStudents, setSelectedStudents] = useState<StudentDTO[]>([]);
 
     return (
-        <StudentContext.Provider value={{ students, setStudents }}>
+        <StudentContext.Provider value={{ students, setStudents, selectedStudents, setSelectedStudents }}>
             {children}
         </StudentContext.Provider>
     );
