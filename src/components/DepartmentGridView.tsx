3f15e29fbb8a4e0931daf85ac6dc6c9da2bf5a44
selim-olmaz <selimolmaz3.1415@gmail.com>
2024-11-26 17:26:49 +0300

    burada departmanları butonlar olarak göstemiyoruz artık. Bunun yerine artık deparmanlar kartlar olarak listelenecek ve department detail section da yapılan işler bu kartta yapılacak

--- src/components/DepartmentGridView.tsx
+++ src/components/DepartmentGridView.tsx
@@ -2,9 +2,7 @@ import * as React from 'react';
 import Grid from '@mui/material/Grid2';
 import Box from '@mui/material/Box';
 import DepartmentDTO from '../models/DepartmentDTO';
-import { useContext } from 'react';
-import { DepartmentContext } from '../context/DepartmentContext';
-import { Button, ButtonBase, ButtonGroup, colors, StepButton } from '@mui/material';
+import DepartmentCardView from './DepartmentCardView';
 
 
 
@@ -13,14 +11,12 @@ interface DepartmentTreeProps {
 }
 
 export default function DepartmentGridView({ departments }: DepartmentTreeProps) {
-    const { setSelectedDepartment } = useContext(DepartmentContext);
-
     return (
         <Box sx={{ width: '100%', margin: 2}}>
             <Grid container rowSpacing={1} columnSpacing={{ xs: 20, sm: 2, md: 3 }}>
                 {departments.map((department) => (
                     <Grid key={department.deptName}>
-                        <Button onClick={() => setSelectedDepartment(department.deptName)} id={department.deptName}>{department.deptName}</Button>
+                        <DepartmentCardView department={department} />
                     </Grid>
                 ))}
             </Grid>
