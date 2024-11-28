cf38bfef7641679e15ecc0b118b23f359b3faca6
selim-olmaz <selimolmaz3.1415@gmail.com>
2024-11-28 05:14:20 +0300

    swiper eklendi

--- src/contents/DepartmentContent.tsx
+++ src/contents/DepartmentContent.tsx
@@ -1,8 +1,13 @@
 import React, { useEffect, useContext, Suspense } from "react";
 import DepartmentService from "../services/DepartmentService";
-import DepartmentStackView from "../components/department/DepartmentStackView";
+import DepartmentCardView from "../components/department/DepartmentCardView";
 import { DepartmentContext } from "../context/DepartmentContext";
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import { Swiper, SwiperSlide, } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 
 function DepartmentContent() {
     const { departments, setDepartments, setSelectedDepartment } = useContext(DepartmentContext);
@@ -11,23 +16,47 @@ function DepartmentContent() {
     useEffect(() => {
         departmentService.getDepartments()
             .then(data => {
-                setDepartments(data); 
-                setSelectedDepartment(data[0]);}) // başlangıçta seçili olan departman
+                setDepartments(data);
+                setSelectedDepartment(data[0]);
+            }) // başlangıçta seçili olan departman
             .catch(error => console.error("Error fetching departments:", error));
     }, [setDepartments]);
 
-
     return (
-        <div>
-            <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+        <Box>
+            <Typography variant="h5" component="h1" sx={{ mb: 2 }}>
                 Departments
             </Typography>
-            {departments.length === 0 ? (
-                <div>Loading...</div>
-            ) : (
-                <DepartmentStackView departments={departments} />
-            )}
-        </div>
+            <Typography variant="h4" component="h1" sx={{ mb: 2 } }>
+                <Swiper
+                    spaceBetween={30}
+                    style={{padding: '20px'}}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    breakpoints={{
+                        200: {
+                            slidesPerView: 1,
+                            spaceBetween: 20,
+                        },
+                        768: {
+                            slidesPerView: 2,
+                            spaceBetween: 30,
+                        },
+                        1024: {
+                            slidesPerView: 3,
+                            spaceBetween: 30,
+                        },
+                    }}
+                    modules={[Pagination]}
+                >
+                    {departments.map((department, index) => (
+                        <SwiperSlide key={index}>
+                            <DepartmentCardView key={department.deptName} department={department} />
+                        </SwiperSlide>))}
+                </Swiper>
+            </Typography>
+        </Box>
     );
 }
 export default DepartmentContent;
