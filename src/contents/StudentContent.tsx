999ed970e1742048dd5c9480fccb60935d6ac4a0
selim-olmaz <selimolmaz3.1415@gmail.com>
2024-11-29 00:44:04 +0300

    basit bir görsel düzeltme

--- src/contents/StudentContent.tsx
+++ src/contents/StudentContent.tsx
@@ -22,7 +22,7 @@ const StudentContent: React.FC = () => {
     return (
         <>
             <Suspense fallback={<div>Loading...</div>}>
-                <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+                <Typography variant="h5" component="h1" sx={{ mb: 2 }}>
                     Student Information of {selectedDepartment.deptName}
                 </Typography>
                 <Typography variant="h5" component="h2" sx={{ mb: 2 }}>
